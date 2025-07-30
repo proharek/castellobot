64f9f46578aa8ffc4cc10c763e3b3593e8b2eed5
proharek <88170170+proharek@users.noreply.github.com>
2025-07-30 22:04:52 +0300

    Update index.js

--- CastelloBot_Render/cloudflare-worker/index.js
+++ CastelloBot_Render/cloudflare-worker/index.js
@@ -1,11 +1,15 @@
 export default {
+  async fetch(request, env, ctx) {
+    const response = await fetch("https://castellobot.onrender.com/healthz");
+    return new Response(`Manual ping status: ${response.status}`);
+  },
+
   async scheduled(event, env, ctx) {
-    console.log("✅ Cron triggered, pinging bot...");
     try {
-      const response = await fetch("https://38830963-534c-403b-9ec1-a15113d21b13-00-1oo4rn2lkznb5.janeway.replit.dev");
-      console.log(`Response status: ${response.status}`);
+      const response = await fetch("https://castellobot.onrender.com/healthz");
+      console.log(`✅ Auto ping status: ${response.status}`);
     } catch (e) {
-      console.error("Ошибка при пинге бота:", e);
+      console.error("❌ Ошибка при авто-пинге:", e);
     }
   }
 }
