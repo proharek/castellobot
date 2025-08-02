7c08f484345dd7aa0fa246a9f96b68aa65963901
proharek <88170170+proharek@users.noreply.github.com>
2025-08-02 04:27:23 +0300

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -1,16 +1,1 @@
-import subprocess
-import time
 
-# Путь к файлу бота
-MAIN_SCRIPT = "main.py"
-
-while True:
-    print("🛡️ Watchdog: запускаю бота...")
-    try:
-        process = subprocess.Popen(["python3", MAIN_SCRIPT])
-        process.wait()
-        print("⚠️ Бот завершился. Перезапуск через 5 секунд...")
-        time.sleep(5)
-    except Exception as e:
-        print(f"❌ Ошибка Watchdog: {e}")
-        time.sleep(5)
